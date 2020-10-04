4fd78bf93bc63b3f479037435752a99dd93d623e
ayindesegun <ayindesegun17gmail.com>
2020-10-04 21:52:55 +0100

    Updates

---
+++ precache-manifest.489c7231a6bc110723375951a127cf41.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cca9ccfabb69bb387f8baa44fa898c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "765d30fa3ca058ecd3bb",
+    "url": "/static/css/main.c7688c40.chunk.css"
+  },
+  {
+    "revision": "e6076259839f6b689fd8",
+    "url": "/static/js/2.a63107ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a63107ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "765d30fa3ca058ecd3bb",
+    "url": "/static/js/main.f404f8c6.chunk.js"
+  },
+  {
+    "revision": "ee1449702b1967514374",
+    "url": "/static/js/runtime-main.25dd52de.js"
+  },
+  {
+    "revision": "494ab46184608ae26ac538ebf5209bfc",
+    "url": "/static/media/rainy bg.494ab461.jpg"
+  },
+  {
+    "revision": "191300c9d28dc3b8f1323393fa1b572e",
+    "url": "/static/media/warm bg.191300c9.jpg"
+  }
+]);
